f78d56786ce33f00b50e77cafcb825578aad1174
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-22 14:47:52 +0600

    add route group for different page

--- New folder_3/routing-and-page-render/app/(content)/news/[slug]/@modal/(.)image/page.js
+++ New folder_3/routing-and-page-render/app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -1,7 +1,10 @@
+'use client';
+
 import { DUMMY_NEWS } from '@/dummy-news';
-import { notFound } from 'next/navigation';
+import { notFound, useRouter } from 'next/navigation';
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
   const newsItemImage = params.slug;
   const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsItemImage);
 
@@ -9,7 +12,7 @@ export default function InterceptedImagePage({ params }) {
 
   return (
     <>
-      <div className="modal-backdrop"></div>
+      <div className="modal-backdrop" onClick={router.back}></div>
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
