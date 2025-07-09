e01b29d638d1165a802cc71ef97e443e4fbb4d63
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-07-09 16:25:08 +0600

    understanding the data cache and cache settings

--- New folder_4/caching/backend/app.js
+++ New folder_4/caching/backend/app.js
@@ -1,14 +1,21 @@
 import express from 'express';
 
 const app = express();
+const port = 8080;
+
+console.log('server is running...');
 
 app.get('/messages', (req, res) => {
   const requestSource = req.headers['x-id'];
-  console.log(`${new Date().toISOString()}: EXECUTING /messages on backend from ${requestSource}`);
+  console.log(
+    `${new Date().toISOString()}: EXECUTING /messages on backend from ${requestSource}`
+  );
   res.json([
     { id: 1, text: 'Hello World' },
     { id: 2, text: 'Another message from the separate backend' },
   ]);
 });
 
-app.listen(8080);
+app.listen(port, () => {
+  console.log('✅ Server is running...');
+});
